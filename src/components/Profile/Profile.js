363c29971aec5bf438dbf81a771ef9b2455cee85
Bailey Niemiller <baileyniemiller@gmail.com>
2020-07-13 14:48:12 -0500

    working on profile styling and alerts

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,6 +6,8 @@ import './Profile.css';
 import HighlightOffIcon from "@material-ui/icons/HighlightOff";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import swal from 'sweetalert';
+import ScrollArea from 'react-scrollbar';
 
 
 // Profile page contains -->
@@ -72,38 +74,38 @@ class ProfilePage extends Component {
         <Nav />
         <h1 id="welcome">Hey, {this.props.user.username}!</h1>
         <Grid container direction="row" justify="flex-end" alginItems="center" spacing={1} className="mainGrid">
-          <Grid item xs={9} sm={9} md={3} xl={3}>
-            <Paper className="favPaper" elevation={2}>
-              <h2>Favorites</h2>
-              <ul>
-                {this.state.favorites.map((place) => (
-                  <div key={place.list_id}>
-                    <li>{place.name}</li>
-                    <HighlightOffIcon onClick={() => {this.handleDeleteFav(place)}}/>
-                  </div>
-                ))}
-              </ul>
-            </Paper>
+          <Grid item xs={9} sm={9} md={3} lg={3} xl={2}>
+              <Paper className="favPaper" elevation={3}>
+                <h2 className="favTitle">Favorites</h2>
+                <ul>
+                  {this.state.favorites.map((place) => (
+                    <div key={place.list_id} className="favDiv">
+                      <li>{place.name}</li>
+                      <HighlightOffIcon onClick={() => {this.handleDeleteFav(place)}}/>
+                    </div>
+                  ))}
+                </ul>
+              </Paper>
           </Grid>
-          <Grid item xs={9} sm={9} md={3} xl={3}>
-            <Paper className="wantPaper" elevation={2}>
-              <h2>Want-To-Go's</h2>
-              <ul>
-                {this.state.wants.map((place) => (
-                  <div key={place.list_id}>
-                    <li>{place.name}</li>
-                    <HighlightOffIcon onClick={() => {this.handleDeleteWant(place)}}/>
-                  </div>
-                ))}
-              </ul>
-            </Paper>
-          </Grid>
-          <Grid item xs={9} sm={9} md={3} xl={3}>
-            <Paper className="noPaper" elevation={2}>       
-              <h2>No-Go's</h2>
+            <Grid item xs={9} sm={9} md={3} lg={3} xl={2}>
+              <Paper className="wantPaper" elevation={3}>
+                <h2 className="wantTitle">Want-To-Go's</h2>
+                  <ul>
+                    {this.state.wants.map((place) => (
+                      <div key={place.list_id} className="wantDiv">
+                        <li>{place.name}</li>
+                        <HighlightOffIcon onClick={() => {this.handleDeleteWant(place)}}/>
+                      </div>
+                    ))}
+                  </ul>
+                </Paper>
+            </Grid>
+          <Grid item xs={9} sm={9} md={3} lg={3} xl={2}>
+            <Paper className="noPaper" elevation={3}>       
+              <h2 className="noTitle">No-Go's</h2>
               <ul>
                 {this.state.nogos.map((place) => (
-                  <div key={place.list_id}>
+                  <div key={place.list_id} className="noDiv">
                     <li>{place.name}</li>
                     <HighlightOffIcon onClick={() => {this.handleDeleteNogo(place)}}/>
                   </div>
